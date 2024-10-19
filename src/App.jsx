9884f3476ed82461b25388343e92cd01dac0f3f6
Carl Freyer <ckf225@lehigh.edu>
2024-10-19 18:27:06 -0400

    map

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
-import { useState, useEffect, useMemo } from "react";
-import {
-  Authenticator,
-  Button,
-  Text,
-  TextField,
-  Heading,
-  Flex,
-  View,
-  Image,
-  Grid,
-  Divider,
-} from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
-import { getUrl } from "aws-amplify/storage";
-import { uploadData } from "aws-amplify/storage";
 import { generateClient } from "aws-amplify/data";
 import outputs from "../amplify_outputs.json";
-import { MapView } from '@aws-amplify/ui-react-geo';
-import { Source, Layer } from 'react-map-gl';
-import {heatmapLayer} from './mapStyle';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Navbar from "./Navbar.jsx";
+import Map from "./Map.jsx";
+import Chat from "./Chat.jsx";
 
 /**
  * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
@@ -30,70 +20,14 @@ const client = generateClient({
   authMode: "userPool",
 });
 
-function filterFeaturesByDay(featureCollection, time) {
-  const date = new Date(time);
-  const year = date.getFullYear();
-  const month = date.getMonth();
-  const day = date.getDate();
-  const features = featureCollection.features.filter(feature => {
-    const featureDate = new Date(feature.properties.time);
-    return (
-      featureDate.getFullYear() === year &&
-      featureDate.getMonth() === month &&
-      featureDate.getDate() === day
-    );
-  });
-  return {type: 'FeatureCollection', features};
-}
-
-export default function App() {const [allDays, useAllDays] = useState(true);
-  const [timeRange, setTimeRange] = useState([0, 0]);
-  const [selectedTime, selectTime] = useState(0);
-  const [earthquakes, setEarthQuakes] = useState(null);
-
-  useEffect(() => {
-    /* global fetch */
-    fetch('https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson')
-      .then(resp => resp.json())
-      .then(json => {
-        // Note: In a real application you would do a validation of JSON data before doing anything with it,
-        // but for demonstration purposes we ingore this part here and just trying to select needed data...
-        const features = json.features;
-        const endTime = features[0].properties.time;
-        const startTime = features[features.length - 1].properties.time;
-
-        setTimeRange([startTime, endTime]);
-        setEarthQuakes(json);
-        selectTime(endTime);
-      })
-      .catch(err => console.error('Could not load data', err)); // eslint-disable-line
-  }, []);
-
-  const data = useMemo(() => {
-    return allDays ? earthquakes : filterFeaturesByDay(earthquakes, selectedTime);
-  }, [earthquakes, allDays, selectedTime]);
-
-  /*
-    <Authenticator>
-      {({ signOut }) => (
-          <Button onClick={signOut}>Sign Out</Button>
-      )}
-    </Authenticator>
-  */
+export default function App() {
   return (
-    <div>
-      <p>Test</p>
-      <MapView initialViewState={{
-            latitude: 40,
-            longitude: -100,
-            zoom: 3
-          }}>
-            {data && (
-            <Source type="geojson" data={data}>
-              <Layer {...heatmapLayer} />
-            </Source>
-          )}
-      </MapView>
-    </div>
+    <Router>
+        <Navbar/>
+        <Routes>
+            <Route exact path="/Map" element={<Map />} />
+            <Route exact path="/Chat" element={<Chat />} />
+        </Routes>
+    </Router>
   );
 }
